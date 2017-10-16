fad73fb7969209d289cd45a9f28b89457c908920
piyushladani <ladanipiyush00@gmail.com>
2017-10-16 14:48:42 +0200

    first commit

---
+++ src/main/java/com/springmvc/dao/UserProfileDao.java
@@ -0,0 +1,15 @@
+package com.springmvc.dao;
+
+import java.util.List;
+
+import com.springmvc.model.UserProfile;
+
+
+public interface UserProfileDao {
+
+	List<UserProfile> findAll();
+	
+	UserProfile findByType(String type);
+	
+	UserProfile findById(int id);
+}
